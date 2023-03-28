351be4158fc0bd7875a5cb84bd42a2ca4f654783
Benji Vesterby <benji@devnw.com>
2023-03-27 20:21:07 -0400

    ci(build.yml): add corrected fuzz script and curl command

--- fuzz.sh
+++ fuzz.sh
@@ -8,7 +8,7 @@ files=$(grep -r --include='**_test.go' --files-with-matches 'func Fuzz' .)
 
 for file in ${files}
 do
-	funcs=$(grep -oP 'func \K(Fuzz\w*)' $file)
+	funcs=$(grep -o 'func Fuzz\w*' $file | sed 's/func //')
 	for func in ${funcs}
 	do
 		echo "Fuzzing $func in $file"
